4eadc2e11b9411b369d097a171ceb7452e6c132d
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-09 23:32:21 +0530

    Added Initial Routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 // Import the Mongoose connection object from a separate module
 const db = require("./config/mongoose");
+// Import the application routes from a separate module
+const routes = require("./routes/index");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const PORT = 7000;
 
 require("dotenv").config();
 
@@ -14,6 +17,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
+// Mount the application routes at the root path
+app.use("/", routes);
+
+app.listen(PORT || process.env.PORT, () => {
+  console.log(`Listening on port ${PORT || process.env.PORT}`);
 });
