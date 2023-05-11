11da6d9ea32089f53d3d05241d3a72a0dc0b6c14
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-11 22:59:40 +0530

    Added User Route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // Define routes for authentication and task management
 router.use("/api/auth", require("./auth"));
+router.use("/api/auth", require("./user"));
 // router.use("/api/tasks", require("./taskRoutes"));
 
 // Export the router from the module
