3da4b433977b1e0cc0fcda03374ff94b062ec55c
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-12 22:58:17 +0530

    Added Auth Middleware for Authentication

--- firebase/index.js
+++ firebase/index.js
@@ -1,10 +1,11 @@
-
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ecommerce-f5415-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL:
+    "https://ecommerce-f5415-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
+module.exports = admin;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+//Import Middleware
+const { authCheck } = require("../middlewares/auth");
+
+//Controller
 const { createOrUpdateUser } = require("../controllers/AuthController");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
