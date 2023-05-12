17f9d84945c758c25a32490b2198e15c41cfd2bb
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-12 22:48:49 +0530

    Added Firebase Admin

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-f5415-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
