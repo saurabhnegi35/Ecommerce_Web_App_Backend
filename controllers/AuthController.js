11f18ba2a23c987db4d4756c2af50208c94e0493
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-17 21:25:51 +0530

    Added currentUser controller in AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 
-module.exports.createOrUpdateUser = async (req, res) => {
+exports.createOrUpdateUser = async (req, res) => {
   const { name, picture, email } = req.user;
 
   const user = await User.findOneAndUpdate(
@@ -21,4 +21,11 @@ module.exports.createOrUpdateUser = async (req, res) => {
     console.log("USER CREATED", newUser);
     res.json(newUser);
   }
+};
+
+exports.currentUser = async (req, res) => {
+  User.findOne({ email: req.user.email }).exec((err, user) => {
+    if (err) throw new Error(err);
+    res.json(user);
+  });
 };
